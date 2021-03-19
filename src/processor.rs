9a616a3af8f02ca6b9ec7da92262919f551da1d1
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-03-19 22:16:10 +0300

    Add process_express_checkout + use borsch for merchant acount

--- src/processor.rs
+++ src/processor.rs
@@ -1,17 +1,18 @@
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint::ProgramResult,
-    program_error::ProgramError,
-    msg,
-    pubkey::Pubkey,
-    program_pack::{Pack, IsInitialized},
-    sysvar::{rent::Rent, Sysvar},
-};
 use crate::{
     error::PaymentProcessorError,
     instruction::PaymentProcessorInstruction,
-    state::{ MerchantAccount },
+    state::{MerchantAccount, Serdes},
 };
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    program_pack::{IsInitialized},
+    pubkey::Pubkey,
+    sysvar::{rent::Rent, Sysvar},
+};
+use spl_token;
 
 /// Processes an instruction
 pub fn process_instruction(
@@ -26,13 +27,24 @@ pub fn process_instruction(
             msg!("Instruction: RegisterMerchant");
             process_register_merchant(program_id, accounts)
         }
+        PaymentProcessorInstruction::ExpressCheckout {
+            amount,
+            merchant_token_pubkey,
+            order_id,
+        } => {
+            msg!("Instruction: ExpressCheckout");
+            process_express_checkout(
+                program_id,
+                accounts,
+                amount,
+                merchant_token_pubkey,
+                order_id,
+            )
+        }
     }
 }
 
-pub fn process_register_merchant(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-) -> ProgramResult {
+pub fn process_register_merchant(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
 
     let signer_info = next_account_info(account_info_iter)?;
@@ -56,7 +68,7 @@ pub fn process_register_merchant(
     }
 
     // get the merchant account
-    let mut merchant_account = MerchantAccount::unpack_unchecked(&merchant_info.data.borrow())?;
+    let mut merchant_account = MerchantAccount::unpack(&merchant_info.data.borrow())?;
     if merchant_account.is_initialized() {
         return Err(ProgramError::AccountAlreadyInitialized);
     }
@@ -64,8 +76,40 @@ pub fn process_register_merchant(
     // create the MerchantAccount object
     msg!("Saving merchant account information...");
     merchant_account.is_initialized = true;
-    merchant_account.merchant_pubkey = *merchant_info.key;
-    MerchantAccount::pack(merchant_account, &mut merchant_info.data.borrow_mut())?;
+    merchant_account.merchant_pubkey = merchant_info.key.to_bytes();
+    MerchantAccount::pack(&merchant_account, &mut merchant_info.data.borrow_mut());
+
+    Ok(())
+}
+
+pub fn process_express_checkout(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    amount: u64,
+    merchant_token_pubkey: [u8; 32],
+    order_id: [u8; 32],
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let signer_info = next_account_info(account_info_iter)?;
+    let payer_token_info = next_account_info(account_info_iter)?;
+    let merchant_acc_info = next_account_info(account_info_iter)?;
+    let rent_sysvar_info = next_account_info(account_info_iter)?;
+    let token_program_info = next_account_info(account_info_iter)?;
+    let rent = &Rent::from_account_info(rent_sysvar_info)?;
+
+    // ensure signer can sign
+    if !signer_info.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+    // ensure payer token account is owned by this program
+    if *payer_token_info.owner != *program_id {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    // ensure merchant account is owned by this program
+    if *merchant_acc_info.owner != spl_token::id() {
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
     Ok(())
 }
