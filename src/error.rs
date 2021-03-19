567f3b0ab04081abdcef53fc6869285ac765c6c3
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-03-19 22:36:25 +0300

    Implement PrintProgramError

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,10 @@
 use thiserror::Error;
-use solana_program::program_error::ProgramError;
+use num_traits::FromPrimitive;
+use solana_program::{
+    decode_error::DecodeError,
+    msg,
+    program_error::{PrintProgramError, ProgramError}
+};
 
 #[derive(Error, Debug, Copy, Clone)]
 pub enum PaymentProcessorError {
@@ -14,5 +19,17 @@ pub enum PaymentProcessorError {
 impl From<PaymentProcessorError> for ProgramError {
     fn from(e: PaymentProcessorError) -> Self {
         ProgramError::Custom(e as u32)
+    }
+}
+
+impl PrintProgramError for PaymentProcessorError {
+    fn print<E>(&self)
+    where
+        E: 'static + std::error::Error + DecodeError<E> + PrintProgramError + FromPrimitive,
+    {
+        match self {
+            PaymentProcessorError::InvalidInstruction => msg!("Error: Invalid Instruction"),
+            PaymentProcessorError::NotRentExempt => msg!("Error: Not Rent Exempt"),
+        }
     }
 }
