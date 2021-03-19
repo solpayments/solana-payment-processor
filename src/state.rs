8abf33aa69ca4b880da7a4ae04280cccfe1cc7aa
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-03-19 22:19:49 +0300

    Clean up + implement serdes for OrderAccount

--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,8 @@
-use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     clock::UnixTimestamp,
     program_error::ProgramError,
-    program_pack::{IsInitialized, Pack, Sealed},
-    pubkey::Pubkey,
+    program_pack::{IsInitialized, Sealed},
 };
 
 pub type PublicKey = [u8; 32];
@@ -31,14 +29,14 @@ pub enum OrderStatus {
     Paid = 2,
 }
 
-#[derive(Debug)]
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq)]
 pub struct OrderAccount {
     pub status: u8,
     pub created: UnixTimestamp,
     pub modified: UnixTimestamp,
-    pub merchant_account: Pubkey,
-    pub mint_pubkey: Pubkey, // represents the token/currency in use
-    pub payer_pubkey: Pubkey,
+    pub merchant_account: PublicKey,
+    pub mint_pubkey: PublicKey, // represents the token/currency in use
+    pub payer_pubkey: PublicKey,
     pub order_id: Vec<u8>,
     pub expected_amount: u64,
     pub paid_amount: u64,
@@ -63,80 +61,14 @@ impl MerchantAccount {
 // impl for OrderAccount
 impl Sealed for OrderAccount {}
 
+impl Serdes for OrderAccount {}
+
 impl IsInitialized for OrderAccount {
     fn is_initialized(&self) -> bool {
         self.status != OrderStatus::Uninitialized as u8
     }
 }
 
-// impl Pack for OrderAccount {
-//     const LEN: usize = 145;
-//     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
-//         let src = array_ref![src, 0, OrderAccount::LEN];
-//         let (
-//             status,
-//             created,
-//             modified,
-//             merchant_account,
-//             mint_pubkey,
-//             payer_pubkey,
-//             order_id,
-//             expected_amount,
-//             paid_amount,
-//             fee_amount,
-//         ) = array_refs![src, 1, 8, 8, 32, 32, 32, 8, 8, 8, 8];
-
-//         Ok(OrderAccount {
-//             status: u8::from_le_bytes(*status),
-//             created: i64::from_le_bytes(*created),
-//             modified: i64::from_le_bytes(*modified),
-//             merchant_account: Pubkey::new_from_array(*merchant_account),
-//             mint_pubkey: Pubkey::new_from_array(*mint_pubkey),
-//             payer_pubkey: Pubkey::new_from_array(*payer_pubkey),
-//             order_id: Vec::<u8>::from(*order_id),
-//             expected_amount: u64::from_le_bytes(*expected_amount),
-//             paid_amount: u64::from_le_bytes(*paid_amount),
-//             fee_amount: u64::from_le_bytes(*fee_amount),
-//         })
-//     }
-
-//     fn pack_into_slice(&self, dst: &mut [u8]) {
-//         let dst = array_mut_ref![dst, 0, OrderAccount::LEN];
-//         let (
-//             status_dst,
-//             created_dst,
-//             modified_dst,
-//             merchant_account_dst,
-//             mint_pubkey_dst,
-//             payer_pubkey_dst,
-//             order_id_dst,
-//             expected_amount_dst,
-//             paid_amount_dst,
-//             fee_amount_dst,
-//         ) = mut_array_refs![dst, 1, 8, 8, 32, 32, 32, 8, 8, 8, 8];
-
-//         let OrderAccount {
-//             status,
-//             created,
-//             modified,
-//             merchant_account,
-//             mint_pubkey,
-//             payer_pubkey,
-//             order_id,
-//             expected_amount,
-//             paid_amount,
-//             fee_amount,
-//         } = self;
-
-//         *status_dst = status.to_le_bytes();
-//         *created_dst = created.to_le_bytes();
-//         *modified_dst = modified.to_le_bytes();
-//         merchant_account_dst.copy_from_slice(merchant_account.as_ref());
-//         mint_pubkey_dst.copy_from_slice(mint_pubkey.as_ref());
-//         payer_pubkey_dst.copy_from_slice(payer_pubkey.as_ref());
-//         *order_id_dst = order_id.as_slice();
-//         *expected_amount_dst = expected_amount.to_le_bytes();
-//         *paid_amount_dst = paid_amount.to_le_bytes();
-//         *fee_amount_dst = fee_amount.to_le_bytes();
-//     }
-// }
+impl OrderAccount {
+    pub const LEN: usize = 145;
+}
