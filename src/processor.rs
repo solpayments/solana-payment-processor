d1e376029a5062709fc772911a2a977e427af8c5
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-03-23 21:12:07 +0300

    Add order account saving

--- src/processor.rs
+++ src/processor.rs
@@ -1,10 +1,11 @@
 use crate::{
     error::PaymentProcessorError,
     instruction::PaymentProcessorInstruction,
-    state::{MerchantAccount, OrderAccount, Serdes},
+    state::{MerchantAccount, OrderAccount, OrderStatus, Serdes},
 };
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
+    clock::Clock,
     entrypoint::ProgramResult,
     msg,
     program_error::ProgramError,
@@ -30,7 +31,7 @@ pub fn process_instruction(
         }
         PaymentProcessorInstruction::ExpressCheckout {
             amount,
-            merchant_token_pubkey,
+            // merchant_token_pubkey,
             order_id,
         } => {
             msg!("Instruction: ExpressCheckout");
@@ -38,7 +39,7 @@ pub fn process_instruction(
                 program_id,
                 accounts,
                 amount,
-                merchant_token_pubkey,
+                // merchant_token_pubkey,
                 order_id,
             )
         }
@@ -87,7 +88,7 @@ pub fn process_express_checkout(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     amount: u64,
-    merchant_token_pubkey: [u8; 32],
+    // merchant_token_pubkey: [u8; 32],
     order_id: Vec<u8>,
 ) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
@@ -96,16 +97,19 @@ pub fn process_express_checkout(
     let payer_token_info = next_account_info(account_info_iter)?;
     let order_acc_info = next_account_info(account_info_iter)?;
     let merchant_acc_info = next_account_info(account_info_iter)?;
-    let rent_sysvar_info = next_account_info(account_info_iter)?;
     let token_program_info = next_account_info(account_info_iter)?;
+    let clock_sysvar_info = next_account_info(account_info_iter)?;
+    let rent_sysvar_info = next_account_info(account_info_iter)?;
+
     let rent = &Rent::from_account_info(rent_sysvar_info)?;
+    let timestamp = &Clock::from_account_info(clock_sysvar_info)?.unix_timestamp;
 
     // ensure signer can sign
     if !signer_info.is_signer {
         return Err(ProgramError::MissingRequiredSignature);
     }
     // get the merchant account
-    let mut merchant_account = MerchantAccount::unpack(&merchant_acc_info.data.borrow())?;
+    let merchant_account = MerchantAccount::unpack(&merchant_acc_info.data.borrow())?;
     if !merchant_account.is_initialized() {
         return Err(ProgramError::UninitializedAccount);
     }
@@ -130,6 +134,17 @@ pub fn process_express_checkout(
     if order_account.is_initialized() {
         return Err(ProgramError::AccountAlreadyInitialized);
     }
+
+    msg!("Saving order information...");
+    order_account.status = OrderStatus::Paid as u8;
+    order_account.created = *timestamp;
+    order_account.modified = *timestamp;
+    order_account.merchant_account = merchant_acc_info.key.to_bytes();
+    order_account.mint_pubkey = merchant_acc_info.key.to_bytes();
+    order_account.payer_pubkey = signer_info.key.to_bytes();
+    order_account.order_id = order_id;
+    order_account.expected_amount = amount;
+    OrderAccount::pack(&order_account, &mut order_acc_info.data.borrow_mut());
 
     Ok(())
 }
