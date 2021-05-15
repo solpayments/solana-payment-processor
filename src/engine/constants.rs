cfbd08f7f492c36e1ef092708d4a81dedc4ad95d
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-15 10:26:22 +0300

    Change program owner address

--- src/engine/constants.rs
+++ src/engine/constants.rs
@@ -1,3 +1,3 @@
 pub const MERCHANT: &str = "merchant";
 pub const PDA_SEED: &[u8] = b"sol_payment_processor";
-pub const PROGRAM_OWNER: &str = "44fVncfVm5fB8VsRBwVZW75FdR1nSVUKcf9nUa4ky6qN";
+pub const PROGRAM_OWNER: &str = "mosh782eoKyPca9eotWfepHVSKavjDMBjNkNE3Gge6Z";
