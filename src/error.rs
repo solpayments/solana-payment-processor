85f05cae4aac3d5c71b8db51116058fec0b3a791
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-16 13:40:37 +0300

    Add InvalidSubscriptionData, InvalidSubscriptionPackage, InvalidOrder, and NotFullyPaid

--- src/error.rs
+++ src/error.rs
@@ -13,12 +13,24 @@ pub enum PaymentProcessorError {
     /// Invalid instruction
     #[error("Error: Invalid Instruction")]
     InvalidInstruction,
+    /// Invalid Subscription Data
+    #[error("Error: Invalid Subscription Data")]
+    InvalidSubscriptionData,
+    /// Invalid Subscription Package
+    #[error("Error: Invalid Subscription Package")]
+    InvalidSubscriptionPackage,
     /// Seller And Buyer Mints Not The Same
     #[error("Error: Seller And Buyer Mints Not The Same")]
     MintNotEqual,
     /// The Amount Is Already Withdrawn
     #[error("Error: The Amount Is Already Withdrawn")]
     AlreadyWithdrawn,
+    /// The Order Account Is Invalid
+    #[error("Error: The Order Account Is Invalid")]
+    InvalidOrder,
+    /// The Payment Has Not Been Received In Full
+    #[error("Error: The Payment Has Not Been Received In Full")]
+    NotFullyPaid,
     /// The Payment Has Not Yet Been Made
     #[error("Error: The Payment Has Not Yet Been Made")]
     NotPaid,
