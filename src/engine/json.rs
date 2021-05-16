f2f7ef0282e032c1f1a5f1f92cff76c42b0cd574
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-16 13:50:16 +0300

    Add docstrings

--- src/engine/json.rs
+++ src/engine/json.rs
@@ -3,7 +3,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Debug, Deserialize, PartialEq)]
 pub struct Package {
     pub name: String,
+    /// duration of the subscription in seconds
     pub duration: i64,
+    /// the price in full for this subscription option
+    /// e.g. if the duration is 1 hour (3600) then the price is per hour
+    /// e.g. if the duration is 1 month (3600 * 24 * 30) then the price is per month
     pub price: u64,
 }
 
