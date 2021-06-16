6aff64257811b62de30470a816af7c32312c3a52
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-06-16 18:06:55 +0300

    Add verify_subscription_order and get_subscription_package

--- src/engine/common.rs
+++ src/engine/common.rs
@@ -1,13 +1,55 @@
 use crate::engine::json::{OrderSubscription, Package, Packages};
 use crate::error::PaymentProcessorError;
 use crate::state::{MerchantAccount, OrderAccount, OrderStatus, Serdes};
+use murmur3::murmur3_32;
 use serde_json::Error as JSONError;
 use solana_program::{
-    account_info::AccountInfo, msg, program_error::ProgramError, program_pack::IsInitialized,
-    pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
+    program_pack::IsInitialized, pubkey::Pubkey,
 };
 use std::io::Cursor;
-use murmur3::murmur3_32;
+
+/// ensure the order is for the subscription
+pub fn verify_subscription_order(
+    subscription_info: &AccountInfo<'_>,
+    order_account: &OrderAccount,
+) -> ProgramResult {
+    let order_json_data: Result<OrderSubscription, JSONError> =
+        serde_json::from_str(&order_account.data);
+    let expected_subscription = match order_json_data {
+        Err(_error) => return Err(PaymentProcessorError::InvalidSubscriptionData.into()),
+        Ok(data) => data.subscription,
+    };
+    if expected_subscription != subscription_info.key.to_string() {
+        return Err(PaymentProcessorError::WrongOrderAccount.into());
+    }
+    Ok(())
+}
+
+/// Get subscription package
+pub fn get_subscription_package(
+    subscription_name: &str,
+    merchant_account: &MerchantAccount,
+) -> Result<Package, ProgramError> {
+    // ensure the merchant has a subscription by this name
+    // TODO: using split looks janky.  Is it necessary?
+    let name_vec: Vec<&str> = subscription_name.split(":").collect();
+    let package_name = name_vec[1];
+    let merchant_json_data: Result<Packages, JSONError> =
+        serde_json::from_str(&merchant_account.data);
+    let packages = match merchant_json_data {
+        Err(_error) => return Err(PaymentProcessorError::InvalidSubscriptionData.into()),
+        Ok(data) => data.packages,
+    };
+    // NB: if the are duplicates, take the first one --> verified in a test
+    let package = packages
+        .into_iter()
+        .find(|package| package.name == package_name);
+    match package {
+        None => return Err(PaymentProcessorError::InvalidSubscriptionPackage.into()),
+        Some(value) => Ok(value),
+    }
+}
 
 pub fn subscribe_checks(
     program_id: &Pubkey,
@@ -38,15 +80,7 @@ pub fn subscribe_checks(
     // get the order account
     let order_account = OrderAccount::unpack(&order_info.data.borrow())?;
     // ensure this order is for this subscription
-    let order_json_data: Result<OrderSubscription, JSONError> =
-        serde_json::from_str(&order_account.data);
-    let expected_subscription = match order_json_data {
-        Err(_error) => return Err(PaymentProcessorError::InvalidSubscriptionData.into()),
-        Ok(data) => data.subscription,
-    };
-    if expected_subscription != subscription_info.key.to_string() {
-        return Err(PaymentProcessorError::WrongOrderAccount.into());
-    }
+    verify_subscription_order(subscription_info, &order_account)?;
     // ensure we have the right payer
     if signer_info.key.to_bytes() != order_account.payer {
         return Err(PaymentProcessorError::WrongPayer.into());
@@ -59,24 +93,8 @@ pub fn subscribe_checks(
     if merchant_info.key.to_bytes() != order_account.merchant {
         return Err(ProgramError::InvalidAccountData);
     }
-    // ensure the merchant has a subscription by this name
-    // TODO: using split looks janky.  Is it necessary?
-    let name_vec: Vec<&str> = subscription_name.split(":").collect();
-    let package_name = name_vec[1];
-    let merchant_json_data: Result<Packages, JSONError> =
-        serde_json::from_str(&merchant_account.data);
-    let packages = match merchant_json_data {
-        Err(_error) => return Err(PaymentProcessorError::InvalidSubscriptionData.into()),
-        Ok(data) => data.packages,
-    };
-    // NB: if the are duplicates, take the first one --> verified in a test
-    let package = packages
-        .into_iter()
-        .find(|package| package.name == package_name);
-    let package = match package {
-        None => return Err(PaymentProcessorError::InvalidSubscriptionPackage.into()),
-        Some(value) => value,
-    };
+    // get the package
+    let package = get_subscription_package(subscription_name, &merchant_account)?;
     if package.mint != Pubkey::new_from_array(order_account.mint).to_string() {
         return Err(PaymentProcessorError::WrongMint.into());
     }
