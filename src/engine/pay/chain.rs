d593abe32c0c0e4b18bb843cfce172be3cac9e2b
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-06-22 21:25:39 +0300

    Add chain checkout instruction

---
+++ src/engine/pay/chain.rs
@@ -0,0 +1,22 @@
+use crate::engine::pay::utils::{process_order, CheckoutType};
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
+
+pub fn process_chain_checkout(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    amount: u64,
+    order_id: String,
+    secret: String,
+    maybe_data: Option<String>,
+) -> ProgramResult {
+    process_order(
+        program_id,
+        accounts,
+        amount,
+        order_id,
+        secret,
+        maybe_data,
+        CheckoutType::Chain,
+    )?;
+    Ok(())
+}
